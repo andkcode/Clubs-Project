b1621dd219b2333f49e3a788fffe913de3f0ffec
jstand <akiricuk37@gmail.com>
2024-12-26 21:16:11 +0100

    Adding controller for successfully connecting user and api requests.

---
+++ src/main/java/com/group/SpringMVCProject/controller/ClubController.java
@@ -0,0 +1,43 @@
+package com.group.SpringMVCProject.controller;
+
+import com.group.SpringMVCProject.dto.ClubDto;
+import com.group.SpringMVCProject.models.Club;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.group.SpringMVCProject.service.ClubService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+@Controller
+
+public class ClubController {
+    private ClubService clubService;
+
+    @Autowired
+    public ClubController(ClubService clubService) {
+        this.clubService = clubService;
+    }
+
+    @GetMapping("/clubs")
+    public String listClubs(Model model) {
+        List<ClubDto> clubs = clubService.findAllClubs();
+        model.addAttribute("clubs",clubs);
+        return "clubs-list";
+    }
+
+    @GetMapping("/clubs/new")
+    public String createClubForm(Model model) {
+        Club club = new Club();
+        model.addAttribute("club",club);
+        return "clubs-create";
+    }
+
+    @PostMapping("/clubs/new")
+    public String saveClub(@ModelAttribute("club") Club club) {
+        clubService.saveClub(club);
+        return "redirect:/clubs";
+    }
+}
