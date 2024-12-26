4f819c977ca7c50b789b922725e1cad83da5d9e3
jstand <akiricuk37@gmail.com>
2024-12-26 21:14:47 +0100

    - Deleting not necessary imports from main class club. - Implementation of dto tool for  working with values from database.

---
+++ src/main/java/com/group/SpringMVCProject/dto/ClubDto.java
@@ -0,0 +1,17 @@
+package com.group.SpringMVCProject.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+public class ClubDto {
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+}
--- src/main/java/com/group/SpringMVCProject/models/Club.java
+++ src/main/java/com/group/SpringMVCProject/models/Club.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 @Data
