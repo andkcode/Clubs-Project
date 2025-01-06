943ef44d297d2de05b97b38c2338de49147baae8
jstand <akiricuk37@gmail.com>
2025-01-06 19:44:28 +0100

    Implemented a feature to display the list of events.

--- src/main/java/com/group/SpringMVCProject/service/EventService.java
+++ src/main/java/com/group/SpringMVCProject/service/EventService.java
@@ -2,7 +2,11 @@ package com.group.SpringMVCProject.service;
 
 import com.group.SpringMVCProject.dto.EventDto;
 
+import java.util.List;
+
 public interface EventService {
 
     void createEvent(Long clubId, EventDto eventDto);
+
+    List<EventDto> findAllEvents();
 }
