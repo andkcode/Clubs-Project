7d03af48fa0b86f373836224dea1882eb12de1f9
jstand <akiricuk37@gmail.com>
2025-01-06 19:43:11 +0100

    - Moved all mappers into separate files to enhance efficiency and maintainability.

---
+++ src/main/java/com/group/SpringMVCProject/mapper/ClubMapper.java
@@ -0,0 +1,34 @@
+package com.group.SpringMVCProject.mapper;
+
+import com.group.SpringMVCProject.dto.ClubDto;
+import com.group.SpringMVCProject.models.Club;
+
+import java.util.stream.Collectors;
+
+import static com.group.SpringMVCProject.mapper.EventMapper.mapToEventDto;
+
+public class ClubMapper {
+    public static Club mapToClub(ClubDto club) {
+        Club clubDto = Club.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createdOn(club.getCreatedOn())
+                .updatedOn(club.getUpdatedOn())
+                .build();
+        return clubDto;
+    }
+
+    public static ClubDto mapToClubDto(Club club) {
+        return ClubDto.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createdOn(club.getCreatedOn())
+                .updatedOn(club.getUpdatedOn())
+                .events(club.getEvents().stream().map((event) -> mapToEventDto(event)).collect(Collectors.toList()))
+                .build();
+    }
+}
