690019f1f539107a9174f46595be002e2b4a6948
andkcode <akiricuk37@gmail.com>
2025-04-04 22:14:27 +0200

    Refactors DTOs by adding Lombok annotations and a blank line.

--- src/main/java/com/group/SpringMVCProject/dto/ClubDto.java
+++ src/main/java/com/group/SpringMVCProject/dto/ClubDto.java
@@ -2,14 +2,18 @@ package com.group.SpringMVCProject.dto;
 
 import com.group.SpringMVCProject.models.UserEntity;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClubDto {
     private Long id;
     @NotEmpty(message = "Club title should not be empty")
--- src/main/java/com/group/SpringMVCProject/dto/EventDto.java
+++ src/main/java/com/group/SpringMVCProject/dto/EventDto.java
@@ -1,29 +1,26 @@
 package com.group.SpringMVCProject.dto;
 
+
 import com.group.SpringMVCProject.models.Club;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
-@Builder
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class EventDto {
     private Long id;
     @NotEmpty(message = "Event name should not be empty")
     private String name;
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-    @NotNull(message = "Start Time should not be empty")
+    @NotEmpty(message = "Start Time should not be empty")
     private LocalDateTime startTime;
-    @NotNull(message = "End Time should not be empty")
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    @NotEmpty(message = "End Time should not be empty")
     private LocalDateTime endTime;
     @NotEmpty(message = "Event type should not be empty")
     private String type;
