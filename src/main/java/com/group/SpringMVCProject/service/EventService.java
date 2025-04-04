f9aca184f454aff4ecd74bca7edef0f1ce807987
andkcode <akiricuk37@gmail.com>
2025-04-05 00:17:37 +0200

    Adds REST API endpoints for event management.

--- src/main/java/com/group/SpringMVCProject/service/EventService.java
+++ src/main/java/com/group/SpringMVCProject/service/EventService.java
@@ -1,18 +1,19 @@
 package com.group.SpringMVCProject.service;
 
 import com.group.SpringMVCProject.dto.EventDto;
+import com.group.SpringMVCProject.models.Event;
 
 import java.util.List;
 
 public interface EventService {
 
-    void createEvent(Long id, EventDto eventDto);
+    Event createEvent(Long id, EventDto eventDto);
 
     List<EventDto> findAllEvents();
 
     EventDto findEventById(Long id);
 
-    void updateEvent(EventDto eventDto);
+    EventDto updateEvent(EventDto eventDto);
 
     void deleteEvent(Long id);
 }
