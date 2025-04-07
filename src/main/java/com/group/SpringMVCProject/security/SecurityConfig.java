d258ee4a36cbee86a3f7c63df3bafff22d8ef7d7
andkcode <akiricuk37@gmail.com>
2025-04-07 22:13:40 +0200

    Adding new requestMatchers to the label for access security permission

--- src/main/java/com/group/SpringMVCProject/security/SecurityConfig.java
+++ src/main/java/com/group/SpringMVCProject/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
                 .and()
                 .csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/clubs", "/clubs/*").permitAll() // ⬅️ Make these public
+                .requestMatchers("/clubs", "/clubs/*", "/events","/events/*").permitAll() // ⬅️ Make these public
                 .requestMatchers("/clubs/admin/**").authenticated() // ⬅️ Admin stuff requires auth
                 .anyRequest().authenticated()
                 .and()
