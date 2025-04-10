6be36e26ad375139c2c7c0a6d7e810e044d158f0
andkcode <akiricuk37@gmail.com>
2025-04-10 15:35:05 +0200

    feat(model): Add Country model
    This commit introduces the `Country` model to the application.  It includes fields for `id` and `name`.  This model will be used to represent countries within the system, enabling features like associating clubs with their respective countries.
    
    The `id` is auto-generated using `GenerationType.IDENTITY`, and the `name` field will store the country's name.  Lombok annotations are used to reduce boilerplate code for getters, setters, constructors, etc.  Jackson annotations are included for future JSON serialization/deserialization needs.  Hibernate annotations are used for automatic timestamp management.  JPA annotations are used for persistence mapping.

---
+++ src/main/java/com/group/SpringMVCProject/models/Country.java
@@ -0,0 +1,24 @@
+package com.group.SpringMVCProject.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="countries")
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
