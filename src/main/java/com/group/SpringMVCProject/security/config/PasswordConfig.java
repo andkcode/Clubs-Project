fd2d129612438a25a9f8bf60edd6bcf65a9b7a16
andkcode <akiricuk37@gmail.com>
2025-05-03 14:31:50 +0200

    refactor(security): Move PasswordConfig to security package
    Moved `PasswordConfig` from `com.group.SpringMVCProject.config` to `com.group.SpringMVCProject.security.config` to better reflect its purpose and improve code organization.  This change consolidates security-related configurations within a dedicated package.

--- src/main/java/com/group/SpringMVCProject/security/config/PasswordConfig.java
+++ src/main/java/com/group/SpringMVCProject/security/config/PasswordConfig.java
@@ -1,4 +1,4 @@
-package com.group.SpringMVCProject.config;
+package com.group.SpringMVCProject.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
