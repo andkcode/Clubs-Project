eac5118f49c56922a8deb3ee8e428391d17b7dc7
andkcode <akiricuk37@gmail.com>
2025-05-03 14:31:26 +0200

    feat(security): Add JwtAuthenticationResponse model
    This commit introduces the `JwtAuthenticationResponse` model, which will be used to encapsulate the JWT token returned to the client upon successful authentication.  This provides a standardized structure for transmitting authentication tokens.
    
    The model includes a single field:
    
    - `token`: The JWT string.

---
+++ src/main/java/com/group/SpringMVCProject/security/models/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.group.SpringMVCProject.security.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtAuthenticationResponse {
+    private String token;
+}
