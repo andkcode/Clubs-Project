752acd57e2f7f8ded8c1bc76710adf65a5bec590
andkcode <akiricuk37@gmail.com>
2025-05-16 18:51:08 +0200

    feat(dto): Add RoleDto class
    This commit introduces a new Data Transfer Object (DTO) called `RoleDto`. This DTO is designed to encapsulate data related to roles within the application, improving data transfer and validation.  It leverages Lombok annotations for conciseness and includes validation annotations (`@NotEmpty`, `@NotNull`) for ensuring data integrity.  The inclusion of `id` and `name` fields allows for comprehensive representation of role information.
    
    The `RoleDto` will be used to transfer role data between the application's layers, enhancing maintainability and reducing redundancy.

---
+++ src/main/java/com/group/SpringMVCProject/dto/RoleDto.java
@@ -0,0 +1,18 @@
+package com.group.SpringMVCProject.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleDto {
+    private Long id;
+    
+    private String name;
+}
